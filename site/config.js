e475a60df3ce1de0cc019b6bda5fb318764102a1
phwt <phwt.smwt@gmail.com>
2019-03-09 16:51:27 +0700

    Initial commit

---
+++ site/config.js
@@ -0,0 +1,50 @@
+buildings = {
+    "drone":{
+        name: "Mining Drone",
+        description: "",
+        base_cost: 20,
+        base_speed: 1
+    },
+    "excavator":{
+        name: "Excavator",
+        description: "",
+        base_cost: 100,
+        base_speed: 4
+    },
+    "boring":{
+        name: "Boring Plant",
+        description: "A vertical boring plant lander. Just boring as that.",
+        base_cost: 700,
+        base_speed: 8
+    },
+    "seismic":{
+        name: "Seismast",
+        description: "Break down the rocks using artificial earthquake.",
+        base_cost: 14000,
+        base_speed: 50
+    },
+    "rods":{
+        name: "God's Rods",
+        description: "",
+        base_cost: 300000,
+        base_speed: 200
+    },
+    "laser":{
+        name: "LASER",
+        description: "Light Assisted Self-Excavating Rig",
+        base_cost: 500000,
+        base_speed: 1500
+    },
+    "7":{
+        name: "7",
+        description: "",
+        base_cost: 50000000,
+        base_speed: 8000
+    },
+    "8":{
+        name: "8",
+        description: "",
+        base_cost: 120000000,
+        base_speed: 40000
+    }
+}
