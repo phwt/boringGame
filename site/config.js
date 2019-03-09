9d4fd4d92d2d8b3ce8936a9eb5b2a360e44cf052
phwt <phwt.smwt@gmail.com>
2019-03-09 23:50:45 +0700

    Add leading number for sorting

--- site/config.js
+++ site/config.js
@@ -1,54 +1,54 @@
 buildings = {
-    "drone":{
+    "1_drone":{
         name: "Mining Drone",
         description: "",
         icon: "unmanned-aerial-vehicle.png",
         base_cost: 20,
         base_speed: 1
     },
-    "excavator":{
+    "2_excavator":{
         name: "Excavator",
         description: "",
         icon: "satellite-dish.png",
         base_cost: 100,
         base_speed: 4
     },
-    "boring":{
+    "3_boring":{
         name: "Boring Plant",
         description: "A vertical boring plant lander. Just boring as that.",
         icon: "satellite-dish.png",
         base_cost: 700,
         base_speed: 8
     },
-    "seismic":{
+    "4_seismic":{
         name: "Seismast",
         description: "Break down the rocks using artificial earthquake.",
         icon: "satellite-dish.png",
         base_cost: 14000,
         base_speed: 50
     },
-    "rods":{
+    "5_rods":{
         name: "God's Rods",
         description: "",
         icon: "satellite-dish.png",
         base_cost: 300000,
         base_speed: 200
     },
-    "laser":{
+    "6_laser":{
         name: "LASER",
         description: "Light Assisted Self-Excavating Rig",
         icon: "satellite-dish.png",
         base_cost: 500000,
         base_speed: 1500
     },
-    "7":{
+    "7_":{
         name: "7",
         description: "",
         icon: "satellite-dish.png",
         base_cost: 50000000,
         base_speed: 8000
     },
-    "8":{
+    "8_":{
         name: "8",
         description: "",
         icon: "satellite-dish.png",
