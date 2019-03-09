1277d02560d33b63ce5a5848ff0a20166bd4dca7
phwt <phwt.smwt@gmail.com>
2019-03-09 18:02:25 +0700

    Add some files

--- site/config.js
+++ site/config.js
@@ -2,48 +2,56 @@ buildings = {
     "drone":{
         name: "Mining Drone",
         description: "",
+        icon: "unmanned-aerial-vehicle.png",
         base_cost: 20,
         base_speed: 1
     },
     "excavator":{
         name: "Excavator",
         description: "",
+        icon: "satellite-dish.png",
         base_cost: 100,
         base_speed: 4
     },
     "boring":{
         name: "Boring Plant",
         description: "A vertical boring plant lander. Just boring as that.",
+        icon: "satellite-dish.png",
         base_cost: 700,
         base_speed: 8
     },
     "seismic":{
         name: "Seismast",
         description: "Break down the rocks using artificial earthquake.",
+        icon: "satellite-dish.png",
         base_cost: 14000,
         base_speed: 50
     },
     "rods":{
         name: "God's Rods",
         description: "",
+        icon: "satellite-dish.png",
         base_cost: 300000,
         base_speed: 200
     },
     "laser":{
         name: "LASER",
         description: "Light Assisted Self-Excavating Rig",
+        icon: "satellite-dish.png",
         base_cost: 500000,
         base_speed: 1500
     },
     "7":{
         name: "7",
         description: "",
+        icon: "satellite-dish.png",
         base_cost: 50000000,
         base_speed: 8000
     },
     "8":{
         name: "8",
         description: "",
+        icon: "satellite-dish.png",
         base_cost: 120000000,
         base_speed: 40000
     }
--- site/scripts.js
+++ site/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    
+//    $(".col").each
+    keys = Object.keys(buildings);
+    for(i=0; i < 8; i++){
+        // console.log(buildings[i]['icon']);
+        console.log('Mww');
+        $("#bldg-slot-area").append(
+            "<div class='bldg-slot row no-gutters'>"+
+                "<div class='col-md-3 text-center'>"+
+                    "<img src='assets/"+ buildings[keys[i]]['icon'] +"'/>"+
+                "</div>"+
+                "<div class='col-md-9'>"+
+                    "<b>"+ buildings[keys[i]]['name'] +"</b><br>"+
+                    "<span class='bldg-cost'>00000</span> KG(s)"+
+                "</div>"+
+            "</div>"
+        );
+    }
 });
