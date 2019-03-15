32bb9b8abf5ea8419d0f6f03b3d3b8d90b5e95c7
phwt <phwt.smwt@gmail.com>
2019-03-15 11:22:43 +0700

    Update config_upgrades.js

--- site/config_upgrades.js
+++ site/config_upgrades.js
@@ -1,7 +1,7 @@
 upgrades = {
     "dig1":{
         name: "Generator Upgrade 1",
-        description: "5kg per dig",
+        description: "5MW per generation cycle",
         icon: "up-arrow.png",
         cost: 200,
         tgt_bldg: "dig",
@@ -9,7 +9,7 @@ upgrades = {
     },
     "dig2":{
         name: "Generator Upgrade 2",
-        description: "100kg per dig",
+        description: "100MW per generation cycle",
         icon: "up-arrow.png",
         cost: 120000,
         tgt_bldg: "dig",
@@ -17,7 +17,7 @@ upgrades = {
     },
     "dig3":{
         name: "Generator Upgrade 3",
-        description: "500kg per dig",
+        description: "500MW per generation cycle",
         icon: "up-arrow.png",
         cost: 400000,
         tgt_bldg: "dig",
@@ -25,7 +25,7 @@ upgrades = {
     },
     "dig4":{
         name: "Generator Upgrade 4",
-        description: "10000kg per dig",
+        description: "10000MW per generation cycle",
         icon: "up-arrow.png",
         cost: 300000,
         tgt_bldg: "dig",
@@ -33,7 +33,7 @@ upgrades = {
     },
     "dig4":{
         name: "Generator Upgrade 4",
-        description: "10000kg per dig",
+        description: "10000MW per generation cycle",
         icon: "up-arrow.png",
         cost: 300000,
         tgt_bldg: "dig",
@@ -41,7 +41,7 @@ upgrades = {
     },
     "biof1":{
         name: "First-generation biofuels",
-        description: "Biofuel Plant x2",
+        description: "Biofuel Plant efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "1_biof",
@@ -49,7 +49,7 @@ upgrades = {
     },
     "biof2":{
         name: "Second-generation biofuels",
-        description: "Biofuel Plant x3",
+        description: "Biofuel Plant efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "1_biof",
@@ -57,7 +57,7 @@ upgrades = {
     },
     "biof3":{
         name: "Third-generation biofuels",
-        description: "Biofuel Plant x4",
+        description: "Biofuel Plant efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "1_biof",
@@ -65,7 +65,7 @@ upgrades = {
     },
     "coal1":{
         name: "Coal Station",
-        description: "Fossil-fuel Power Plant x2",
+        description: "Fossil-fuel Power Plant efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "2_coal",
@@ -73,7 +73,7 @@ upgrades = {
     },
     "coal2":{
         name: "Natural Gas Station",
-        description: "Fossil-fuel Power Plant x3",
+        description: "Fossil-fuel Power Plant efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "2_coal",
@@ -81,7 +81,7 @@ upgrades = {
     },
     "coal3":{
         name: "Petroleum Station",
-        description: "Fossil-fuel Power Plant x4",
+        description: "Fossil-fuel Power Plant efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "2_coal",
@@ -89,7 +89,7 @@ upgrades = {
     },
     "sun1":{
         name: "Fixed Arrays",
-        description: "Photovoltaic Station x2",
+        description: "Photovoltaic Station efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "3_sun",
@@ -97,7 +97,7 @@ upgrades = {
     },
     "sun2":{
         name: "Single-axis Arrays",
-        description: "Photovoltaic Station x3",
+        description: "Photovoltaic Station efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "3_sun",
@@ -105,7 +105,7 @@ upgrades = {
     },
     "sun3":{
         name: "Dual-axis Arrays",
-        description: "Photovoltaic Station x4",
+        description: "Photovoltaic Station efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "3_sun",
@@ -113,7 +113,7 @@ upgrades = {
     },
     "wind1":{
         name: "Vertical-axis Blades",
-        description: "Wind Farm x2",
+        description: "Wind Farm efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "4_wind",
@@ -121,7 +121,7 @@ upgrades = {
     },
     "wind2":{
         name: "Horioztontal-axis Blades",
-        description: "Photovoltaic Station x3",
+        description: "Photovoltaic Station efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "4_wind",
@@ -129,7 +129,7 @@ upgrades = {
     },
     "wind3":{
         name: "Darrieus Blades",
-        description: "Photovoltaic Station x4",
+        description: "Photovoltaic Station efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "4_wind",
@@ -137,7 +137,7 @@ upgrades = {
     },
     "water1":{
         name: "Stainless Blade",
-        description: "Hydroelectric Dam x2",
+        description: "Hydroelectric Dam efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "5_water",
@@ -145,7 +145,7 @@ upgrades = {
     },
     "water2":{
         name: "Nikel Blade",
-        description: "Hydroelectric Dam x3",
+        description: "Hydroelectric Dam efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "5_water",
@@ -153,7 +153,7 @@ upgrades = {
     },
     "water3":{
         name: "Titanium Blade",
-        description: "Hydroelectric Dam x4",
+        description: "Hydroelectric Dam efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "5_water",
@@ -161,7 +161,7 @@ upgrades = {
     },          
     "oil1":{
         name: "Sub-sea Platform",
-        description: "Offshore Rig x2",
+        description: "Offshore Rig efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "6_oil",
@@ -169,7 +169,7 @@ upgrades = {
     },
     "oil2":{
         name: "Fixed Platform",
-        description: "Offshore Rig x3",
+        description: "Offshore Rig efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "6_oil",
@@ -177,7 +177,7 @@ upgrades = {
     },
     "oil3":{
         name: "Semi-submersibles Platform",
-        description: "Offshore Rig x4",
+        description: "Offshore Rig efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "6_oil",
@@ -185,7 +185,7 @@ upgrades = {
     },
     "thermal1":{
         name: "Dry Steam",
-        description: "Geothermal Plant x2",
+        description: "Geothermal Plant efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "7_thermal",
@@ -193,7 +193,7 @@ upgrades = {
     },
     "thermal2":{
         name: "Flash Stream",
-        description: "Geothermal Plant x3",
+        description: "Geothermal Plant efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "7_thermal",
@@ -201,7 +201,7 @@ upgrades = {
     },
     "thermal3":{
         name: "Binary Cycle",
-        description: "Geothermal Plant x4",
+        description: "Geothermal Plant efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "7_thermal",
@@ -209,7 +209,7 @@ upgrades = {
     },
     "nuke1":{
         name: "Pu-239 Reactor",
-        description: "Nuclear Power x2",
+        description: "Nuclear Power efficiency x2",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "8_nuke",
@@ -217,7 +217,7 @@ upgrades = {
     },
     "nuke2":{
         name: "U-235 Reactor",
-        description: "Nuclear Power x3",
+        description: "Nuclear Power efficiency x3",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "8_nuke",
@@ -225,7 +225,7 @@ upgrades = {
     },
     "nuke3":{
         name: "Th-232 Reactor",
-        description: "Nuclear Power x4",
+        description: "Nuclear Power efficiency x4",
         icon: "up-arrow.png",
         cost: 0,
         tgt_bldg: "8_nuke",
